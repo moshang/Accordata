698fd0cd06a4357a9f0892319d95c9ced5a950b8
MoShang <moshangmusic@yahoo.com>
2018-06-28 13:27:08 +0800

    Map is in scroll rect - elastic

--- Assets/ACCORDATA/Scripts/inputController.cs
+++ Assets/ACCORDATA/Scripts/inputController.cs
@@ -18,6 +18,7 @@ public class inputController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         var d = Input.GetAxis("Mouse ScrollWheel");
         if (d > 0f)
         {
@@ -37,26 +38,23 @@ public class inputController : MonoBehaviour
             mapScale.y = scaleFactor;
             mapPanel.localScale = mapScale;
         }
-        if (Input.GetMouseButtonDown(0))
-        {
-            mapStartPos.x = mapPanel.anchoredPosition.x;
-            mapStartPos.y = mapPanel.anchoredPosition.y;
-            mouseDownPos.x = mouseDownPos.x = Input.mousePosition.x; ;
-            mouseDownPos.y = Input.mousePosition.y;
-        }
+        /*
+if (Input.GetMouseButtonDown(0))
+{
+mapStartPos.x = mapPanel.anchoredPosition.x;
+mapStartPos.y = mapPanel.anchoredPosition.y;
+mouseDownPos.x = mouseDownPos.x = Input.mousePosition.x; ;
+mouseDownPos.y = Input.mousePosition.y;
+}
 
-        if (Input.GetMouseButton(0))
-        {
-            /*
-            float scaleFactor = mapScale.x;
-            float minX = utils.map(scaleFactor, 0.8f, 3, 0, 1000);
-            float maxX = utils.map(scaleFactor, 0.8f, 0, 0, -500);
-            */
-            Vector2 newMapPos;
-            //newMapPos.x = Mathf.Clamp(mapStartPos.x + (Input.mousePosition.x - mouseDownPos.x), minX, maxX);
-            newMapPos.x = mapStartPos.x + (Input.mousePosition.x - mouseDownPos.x);
-            newMapPos.y = mapStartPos.y + (Input.mousePosition.y - mouseDownPos.y);
-            mapPanel.anchoredPosition = newMapPos;
-        }
+if (Input.GetMouseButton(0))
+{
+Vector2 newMapPos;
+//newMapPos.x = Mathf.Clamp(mapStartPos.x + (Input.mousePosition.x - mouseDownPos.x), minX, maxX);
+newMapPos.x = mapStartPos.x + (Input.mousePosition.x - mouseDownPos.x);
+newMapPos.y = mapStartPos.y + (Input.mousePosition.y - mouseDownPos.y);
+mapPanel.anchoredPosition = newMapPos;
+}
+*/
     }
 }
