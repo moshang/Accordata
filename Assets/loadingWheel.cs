dc4b43cc2fe9f7ffefa295073ccc695914dca4a3
MoShang <moshangmusic@yahoo.com>
2018-07-02 01:32:39 +0800

    loadingWheel

---
+++ Assets/loadingWheel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loadingWheel : MonoBehaviour
+{
+    public Image[] spokes;
+    private int startSpoke = 12;
+    private Color32 startColor = new Color32(255, 255, 255, 255);
+
+    private void OnEnable()
+    {
+        InvokeRepeating("updateColors", 0, 0.083f);
+    }
+
+    private void updateColors()
+    {
+        startSpoke = (startSpoke - 1 >= 0) ? startSpoke - 1 : 11;
+        for (int i = 0; i < 12; i++)
+        {
+            Color32 newColor = startColor;
+            newColor.a = (byte)(255 - (i * 15));
+            spokes[(startSpoke + i) % 12].color = newColor;
+        }
+    }
+}
